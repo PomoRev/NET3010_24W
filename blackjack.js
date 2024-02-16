b2c9544a849f268cc2c544e7eb7ab78001425884
Dr. Frank Emanuel <emanuef@algonquincollege.com>
2024-02-16 12:45:09 -0500

    deck is now almost complete

--- blackjack.js
+++ blackjack.js
@@ -5,18 +5,20 @@
 
 const JOKER = 0, HEARTS = 1, SPADES = 2, DIAMONDS = 3, CLUBS = 4;
 const ACE = 1, JACK = 11, QUEEN = 12, KING = 13;
+const DECKSIZE = 52;
 const CARDBACK = "cardback.png";
 
 // object definitions 
 
 function Card( cardSuit, cardValue ) {
 
-   /*   This constructor expects a suit value from 0-4 and a value from 1-13, if a 0 is given 
-        for the suit then the value is not important as the card will be a joker.
+   /*   This constructor expects a suit value from 0-4 and a value from 1-13, 
+        if a 0 is given for the suit then the value is not important as 
+        the card will be a joker.
         Cards are meant to be created in a Deck object.   
-        Data:   the card suit, the card value, if it is faceup (default is false), the 
-                filename of the card back image, and the filename of the image for the front 
-                of the card.
+        Data:   the card suit, the card value, if it is faceup (default is false), 
+                the filename of the card back image, and the filename of the image 
+                for the front of the card.
         Methods (interface):
             returnFacing()  - returns true if the card is face up, otherwise returns false.
             flip()          - reverses the facing of the card.
@@ -63,11 +65,12 @@ function Card( cardSuit, cardValue ) {
 
 function Deck( numberDecks, numberJokers ){
 
-    /*  This constructor expects a number of 52 card decks to include in the deck as well as a
-        variable number of jokers to add to the deck.   
-        Data:   the main data is an array containing all of the cards of a deck. Because the use
-                of a deck is destructive, we will also store the number of decks and the number of 
-                jokers in case we need to refresh the deck.
+    /*  This constructor expects a number of 52 card decks to include in 
+        the deck as well as a variable number of jokers to add to the deck.   
+        Data:   the main data is an array containing all of the cards of a deck. 
+                Because the use of a deck is destructive, we will also store 
+                the number of decks and the number of jokers in case we need 
+                to refresh the deck.
         Methods (interface):
             dealCard()  - pops the last card off of the array of cards.
             numberOfCardsRemaining()  - returns the number of cards left in the deck.
@@ -100,11 +103,11 @@ function Deck( numberDecks, numberJokers ){
 
 function Hand( positionX, positionY ){
 
-    /*  This constructor creates an array for holding cards as well as functions to allow the game
-        to work. The constructor expects an X and Y offset for display purposes, this is where the 
-        first card will be displayed.   
-        Data:   The main data is an array of cards. The hand will contain an x and y offset for 
-                positioning the hand on the screen. 
+    /*  This constructor creates an array for holding cards as well as functions 
+        to allow the game to work. The constructor expects an X and Y offset for 
+        display purposes, this is where the first card will be displayed.   
+        Data:   The main data is an array of cards. The hand will contain an x 
+                and y offset for positioning the hand on the screen. 
         Methods (interface):
             setPosition( positionX, positionY )  - allows us to modify the hand position on the display
             numberOfCardsRemaining()  - returns the number of cards left in the hand.
@@ -167,12 +170,12 @@ Deck.prototype.dealCard = function () { return this.deck.pop(); }
 Deck.prototype.numberOfCardsRemaining = function () { return this.deck.length; }
 Deck.prototype.shuffle = function () {
 
-    /*  Fisher Yates Array Shuffle Algorithm (modified)
+        /*  Fisher Yates Array Shuffle Algorithm (modified)
 
-        1.  Write down the numbers from 1 through number of elements
-            in the array.
+        1.  Determine the size of the array, we will start at one end 
+            of the array and move through the array.
         2.  Pick a random number between 1 and the 
-            number of unstruck numbers remaining inclusive of the
+            number of touched numbers remaining inclusive of the
             number of the current element itself.
         3.  Swap the current element with the random element.
         4.  Freeze the current element
@@ -181,11 +184,45 @@ Deck.prototype.shuffle = function () {
     
         The array should be sufficiently randomized. 
     */
+
+    for ( i = (this.deck.length - 1); i > 1; i--) {
+
+        let randomTarget = Math.floor(Math.random() * (i+1));
+
+// console.log ( "swapping " + i + " with " + randomTarget );
+
+        let temp = this.deck[i];
+        this.deck[i] = this.deck[randomTarget];
+        this.deck[randomTarget] = temp;
+
+    }
+
 }
-Deck.muckDeck = function () {
+Deck.prototype.muckDeck = function () {
 
-    // build an algorithm
+    // empty the array
 
+    let numberOfIterations = this.numberOfCardsRemaining();
+
+    for (i = 0; i < numberOfIterations; i++){
+        this.dealCard();
+    }
+
+    // repopulate the deck with a full set of cards and jokers
+
+    for ( i = 0; i < this.numberOfDecks; i++){
+
+        for ( iSuit = 1; iSuit <= 4; iSuit++ ){
+
+            for ( iValue = 1; iValue <= KING; iValue++ ){
+                this.deck.push( new Card(iSuit, iValue));
+            }
+        }
+    }
+
+    for ( i = 0; i < this.numberOfJokers; i++ ){
+        this.deck.push( new Card (JOKER, JOKER) );
+    }
 }
 
 Hand.prototype.setPosition( positionX, positionY ) = function () {
